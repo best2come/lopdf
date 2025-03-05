1cab3dfa7c8634d1fa067dd450bcd19df9affb54
bam5wi <Martin.Bammer2@at.bosch.com>
2025-03-05 14:06:04 +0100

    Enable mimalloc only if feature is enabled

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 #![forbid(unsafe_code)]
 #![deny(clippy::all)]
 
+#[cfg(feature = "mimalloc")]
 #[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
@@ -40,7 +41,7 @@ pub use object::{Dictionary, Object, ObjectId, Stream, StringFormat};
 pub use bookmarks::Bookmark;
 pub use common_data_structures::{decode_text_string, text_string};
 pub use destinations::Destination;
-pub use encodings::{encode_utf16_be, encode_utf8, Encoding};
+pub use encodings::{Encoding, encode_utf8, encode_utf16_be};
 pub use encryption::{EncryptionState, EncryptionVersion, Permissions};
 pub use error::{Error, Result};
 pub use incremental_document::IncrementalDocument;
